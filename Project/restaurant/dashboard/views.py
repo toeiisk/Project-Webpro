26aa6f4170c9d82bf8486a4e8291c48975a2b773
Sukrit leelakornkij <42915021+toeiisk@users.noreply.github.com>
2020-02-27 02:03:31 +0700

    update
    ดึงชื่อคณะได้แล้ว

--- Project/restaurant/dashboard/views.py
+++ Project/restaurant/dashboard/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import redirect, render
 from django.template.context_processors import request
-
+from home.models import Faculty
 
 # Create your views here.
 def dashboard(request):
-    return render (request, 'dashboard/index.html')
+    faculty = Faculty.objects.all()
+    return render (request, 'dashboard/index.html', context={'faculty': faculty})
 
 def edit(request):
     return render (request, 'dashboard/edit.html')
