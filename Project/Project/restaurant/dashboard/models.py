5003bdbf6ae74976c3dc08db7d4f9ef57a8b6864
Sukrit leelakornkij <42915021+toeiisk@users.noreply.github.com>
2020-02-28 00:56:09 +0700

    update new all

--- Project/Project/restaurant/dashboard/models.py
+++ Project/Project/restaurant/dashboard/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Faculty(models.Model):
-    name = models.CharField(max_length=20)
+
     
     
--- Project/Project/restaurant/home/models.py
+++ Project/Project/restaurant/home/models.py
@@ -1,6 +1,4 @@
-
 from django.db import models
-
 
 # Create your models here.
 class Faculty(models.Model):
@@ -15,4 +13,3 @@ class Restaurant(models.Model):
     rating = models.IntegerField()
     approve_by = models.CharField(max_length=20)
     approve_date = models.DateField()
-    faculty_id = models.ForeignKey(Faculty, default=None, on_delete=models.CASCADE)
