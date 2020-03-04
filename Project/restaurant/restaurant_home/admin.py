70ea5e32b0dca2f075978933c21813acd7189c7a
Sukrit leelakornkij <42915021+toeiisk@users.noreply.github.com>
2020-03-05 03:46:26 +0700

    update
    5/3/63

--- Project/restaurant/restaurant_home/admin.py
+++ Project/restaurant/restaurant_home/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Faculty, Restaurant
+from .models import Faculty, Restaurant, RestaurantFood, Food
 from django.contrib.auth.models import Permission
 
 # Register your models here.
 admin.site.register(Permission)
 admin.site.register(Restaurant)
 admin.site.register(Faculty)
+admin.site.register(RestaurantFood)
+admin.site.register(Food)
