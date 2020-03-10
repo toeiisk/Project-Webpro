b87b58a3ee09832896a3dfa88267a2c4d2a6d03c
toeiisk <42915021+toeiisk@users.noreply.github.com>
2020-03-11 01:21:18 +0700

    update

--- Project/restaurant/dashboard/urls.py
+++ Project/restaurant/dashboard/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 from dashboard.views import dashboard, restaurant_add
-from details.urls import *
 
 urlpatterns = [
     path('dashboard/', dashboard, name='dashboard'),
     path('admin/restaurant_home/restaurant/add/', restaurant_add, name='restaurant_add'),
+    path('restaurant/delete/<int:restaurant_id>/', views.restaurant_delete, name='restaurant_delete'),
+    path('admin/restaurant_home/restaurant/<int:restaurant_id>/change/', views.restaurant_list, name='restaurant_list'),
 ]
